b2ded2ec9a741f1bb7200106f24faf4b01e5603d
Abhinav215 <159780645+w4lyf@users.noreply.github.com>
2025-02-08 18:26:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  publicDir: 'public',
-})
+  publicDir: "public",
+});
