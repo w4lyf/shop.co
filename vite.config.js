24d80de46a949a780658b7c832c7fb2f1d57646c
Abhinav215 <159780645+w4lyf@users.noreply.github.com>
2025-02-08 18:32:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",  // Use relative paths
+  base: "./ecommerce-shop.co/",  // Use relative paths
   plugins: [react()],
   publicDir: "public",
 });
