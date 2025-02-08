7b68d92b420046822efdc4a91537f48f8202f75a
Abhinav215 <159780645+w4lyf@users.noreply.github.com>
2025-02-08 18:36:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",  // Use relative paths
+  base: "/",  // Ensure app loads at root
   plugins: [react()],
   publicDir: "public",
 });
