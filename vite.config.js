5280328fb13a50771be9adb67815714cd6218f9b
Abhinav215 <159780645+w4lyf@users.noreply.github.com>
2025-02-08 18:15:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ecommerce-shop.co/",
+  base: "/",
   plugins: [react()],
   publicDir: 'public',
 })
